87917678ec8b8023e5d12e1a22bbc7262ab9855b
ALBERT SADYKOV <albertsadykov@ro.ru>
2025-03-05 14:01:03 +0300

    click lib injected

--- app/index.py
+++ app/index.py
@@ -3,27 +3,33 @@ from os.path import isfile, join
 
 from modules.converter.three_format import generate_tiles
 
-if __name__ == '__main__':
+import click
+@click.command()
+# Каталог иходных панорам
+@click.option('--path', default='./assets/panoramas/', help='Укажите путь до каталога с панорамами')
+@click.option('--results_path', default='./results/', help='Укажите путь до каталог результатов')
+@click.option('--levels', default='3', help='Укажите количество генерируемых уровней качества (целое число)')
+@click.option('--initial_quality', default='0.5', help='Укажите ачальное качество относительно исходного изображения (от 0 до 1)')
+def main (path, results_path, levels, initial_quality):
     # Именование уровней (опционально)
+    level_names = None
     # level_names = {0: "low", 1: "medium", 2: "high"}
-    # Каталог иходных панорам
-    inital_path = './assets/panoramas/'
-    # Каталог результатов
-    results_path = './results/'
-    # Кол-во уровней детализации
-    # (Каждый уровень детализации по качеству хуже в 2 раза, чем предыдущий)
-    levels = 5
-    # Начальное качество относительно исходного изображения
-    initial_quality = 1
+    # level_names = {0: "low", 1: "low-medium", 2: "medium", 3: "medium-high", 4: "high"}
+
+    levels = int(levels)
+    initial_quality = float(initial_quality)
 
     # Парсим пути до панорам
-    images = [f for f in listdir(inital_path) if isfile(join(inital_path, f)) and f.endswith((".jpg", ".jpeg",".gif",".png",".tga", ".webp")) ]
-
-    for i in images[-1:]:
+    images = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith((".jpg", ".jpeg",".gif",".png",".tga", ".webp")) ]
+    
+    for i in images[1:2]:
         generate_tiles(
-            inital_path + i,
-            results_path + ''.join(i.split('.')[:-1]),
-            levels,
-            initial_quality,
-            level_names = None
+            image_path = path + i,
+            result_path = results_path + ''.join(i.split('.')[:-1]),
+            levels = levels,
+            quality = initial_quality,
+            level_names = level_names 
         )
+
+if __name__ == '__main__':
+    main()
